c2e0449236eda2814cbba4864f7b28d0d142ad7c
Philipp Duck <phil.mar.du@gmail.com>
2022-01-16 15:27:35 +0100

    edited README.md

--- src/main/java/duck/philippmarcel/notes/HelloApplication.java
+++ src/main/java/duck/philippmarcel/notes/HelloApplication.java
@@ -17,10 +17,10 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class MainApplication extends Application {
+public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(getClass().getResource("stylesheet.css").toString());
         stage.setTitle("Notes");
